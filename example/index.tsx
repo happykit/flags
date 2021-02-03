68063719744842ec7a5efdd16426c0af6bfaf65b
Dominik Ferber <dominik.ferber@gmail.com>
2021-02-03 17:19:46 +0200

    add wip implementation of full flags responses

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useFlags, configure } from '../.';
+import { useFlags, useFeatureFlags, configure } from '../.';
 
 type Flags = {
   booleanFlag: boolean;
@@ -9,18 +9,27 @@ type Flags = {
   textualFlag: 'profileA' | 'profileB';
 };
 
+const useProd = false;
+
 configure<Partial<Flags>>({
-  // endpoint: 'http://localhost:8787/',
-  envKey: 'flags_pub_277203581177692685',
-  defaultFlags: { booleanFlag: true },
+  endpoint: useProd ? undefined : 'http://localhost:8787/api/flags',
+  envKey: useProd
+    ? 'flags_pub_277203581177692685'
+    : 'flags_pub_272357356657967622',
+  defaultFlags: {},
 });
 
+// const App = () => {
+//   const flags = useFlags<Flags>();
+//   // flags.booleanFlag; // has type "boolean"
+//   // flags.numericFlag; // has type "number"
+//   // flags.textualFlag; // has type "string"
+//   return <pre>{JSON.stringify(flags, null, 2)}</pre>;
+// };
+
 const App = () => {
-  const flags = useFlags<Flags>();
-  // flags.booleanFlag; // has type "boolean"
-  // flags.numericFlag; // has type "number"
-  // flags.textualFlag; // has type "string"
-  return <div>{JSON.stringify(flags, null, 2)}</div>;
+  const response = useFeatureFlags<Flags>();
+  return <pre>{JSON.stringify(response, null, 2)}</pre>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
