60543ea75639c1448bafba3f019347ab326d22a8
Dominik Ferber <dominik.ferber@gmail.com>
2021-04-16 17:53:19 +0300

    replace jest-fetch-mock with fetch-mock-jest

--- package/jest.config.js
+++ package/jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   // automock: false,
-  setupFiles: ["./jest/fetch.js", "./jest/mutation-observer.js"],
+  setupFiles: ["./jest/mutation-observer.js"],
 };
