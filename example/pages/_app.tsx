e916144d4b386535e8109c465db6266784438588
Dominik Ferber <dominik.ferber@gmail.com>
2021-09-03 16:26:52 +0300

    add plausible

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import { configure } from "@happykit/flags/config";
+import Script from "next/script";
 
 configure({
   envKey: process.env.NEXT_PUBLIC_FLAGS_ENV_KEY!,
@@ -17,6 +18,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon.png" />
       </Head>
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          src="https://plausible.io/js/plausible.js"
+          data-domain="flags.happykit.dev"
+        />
+      )}
       <Component {...pageProps} />
     </React.Fragment>
   );
