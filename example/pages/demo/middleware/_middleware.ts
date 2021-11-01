3c56a4866991b6e5d8293849eaf2bc2049a099d5
Dominik Ferber <dominik.ferber@gmail.com>
2021-11-01 06:52:18 +0200

    use response.cookie
    response.cookie was originally broken when Expires was present on the cookie: https://github.com/vercel/next.js/issues/30430
    
    This will be fixed in Next.js v12.0.2, so we use the canary of that for now, and the release of happykit might have to be a breaking change since we then no longer support older Next.js versions theoretically.
    
    We need to list next >=12.0.2 as a peerDependency once v12.0.2 is out.

--- example/pages/demo/middleware/_middleware.ts
+++ example/pages/demo/middleware/_middleware.ts
@@ -1,14 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { getEdgeFlags, hkvkCookieOptions } from "@happykit/flags/edge";
-import { AppFlags } from "../../../types/AppFlags";
+import type { AppFlags } from "../../../types/AppFlags";
 import "../../../happykit.config";
-
-function serializeVisitorKeyCookie(visitorKey: string) {
-  // Max-Age 15552000 seconds equals 180 days
-  return `hkvk=${encodeURIComponent(
-    visitorKey
-  )}; Path=/; Max-Age=15552000; SameSite=Lax`;
-}
 
 export async function middleware(request: NextRequest, ev: NextFetchEvent) {
   const flagBag = await getEdgeFlags<AppFlags>({ request });
@@ -18,15 +11,7 @@ export async function middleware(request: NextRequest, ev: NextFetchEvent) {
   );
 
   if (flagBag.data?.visitor?.key) {
-    // response.cookie has a bug when used with an expiration date,
-    // so we append Set-Cookie manually
-    // https://github.com/vercel/next.js/issues/30430
-    //
-    // response.cookie("hkvk", flagBag.data.visitor.key, hkvkCookieOptions);
-    response.headers.append(
-      "Set-Cookie",
-      serializeVisitorKeyCookie(flagBag.data.visitor.key)
-    );
+    response.cookie("hkvk", flagBag.data.visitor.key, hkvkCookieOptions);
   }
 
   return response;
