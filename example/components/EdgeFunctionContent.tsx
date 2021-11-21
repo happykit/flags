6095fa284f7caccad7f480be350bd898c355f72e
Dominik Ferber <dominik.ferber@gmail.com>
2021-11-21 23:16:29 +0200

    explain middleware behaviour in EdgeFunctionContent

--- example/components/EdgeFunctionContent.tsx
+++ example/components/EdgeFunctionContent.tsx
@@ -12,7 +12,7 @@ export function EdgeFunctionContent(props: {
           href="https://nextjs.org/blog/next-12#introducing-middleware"
           rel="noreferrer noopener"
         >
-          middleware
+          Next.js Middleware
         </a>
         .
       </p>
@@ -32,11 +32,31 @@ export function EdgeFunctionContent(props: {
         Remove cookie and reload
       </button>
       <p>
-        Since this page is served statically from the edge, rendering will use
-        no visitor key. This is necessary as the the concept of a visitor does
+        This example uses a <code>_middleware</code> file at{" "}
+        <code>pages/demo/middleware</code> to statically render different
+        variants for the <code>/demo/middleware</code> path. The different
+        variants live under <code>pages/demo/middleware/variant-*.tsx</code>.
+      </p>
+      <p>
+        The middleware loads the flags and rewrites the incoming request either
+        to <code>variant-short.tsx</code>, <code>variant-medium.tsx</code> or{" "}
+        <code>variant-full.tsx</code> depending on the resolved flag variant.
+      </p>
+      <p>
+        Since resulting page is served statically from the edge, rendering will
+        use no visitor key. This is necessary as the concept of a visitor does
         not exist during static site generation. Thus all rules and
         percentage-based rollouts targeting a visitor resolve to{" "}
         <code>null</code>.
+      </p>
+      <p>
+        The middleware loads the flags purely to decide where to rewrite the
+        request to. It does not send any resolved flags into the application
+        itself.
+      </p>
+      <p>
+        You are however free to call <code>useFlags()</code> on the client and
+        combine this approach with the middleware.
       </p>
     </React.Fragment>
   );
