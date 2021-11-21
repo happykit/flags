2eebbc23c26789c034e2e70ef9046701939467b9
Dominik Ferber <dominik.ferber@gmail.com>
2021-11-21 18:09:23 +0200

    add reload button to middleware

--- example/components/EdgeFunctionContent.tsx
+++ example/components/EdgeFunctionContent.tsx
@@ -20,11 +20,23 @@ export function EdgeFunctionContent(props: {
         You have been served the "<code>{props.checkoutVariant}</code>" checkout
         variant.
       </pre>
+      <button
+        type="button"
+        className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={() => {
+          document.cookie =
+            "hkvk=; path=/; maxAge=0; expires=Thu, 01 Jan 1970 00:00:01 GMT";
+          window.location.reload();
+        }}
+      >
+        Remove cookie and reload
+      </button>
       <p>
-        Since this page is served from the edge. rendering will use no visitor
-        key. This is necessary as the the concept of a visitor does not exist
-        during static site generation. Thus all rules and percentage-based
-        rollouts targeting a visitor resolve to <code>null</code>.
+        Since this page is served statically from the edge, rendering will use
+        no visitor key. This is necessary as the the concept of a visitor does
+        not exist during static site generation. Thus all rules and
+        percentage-based rollouts targeting a visitor resolve to{" "}
+        <code>null</code>.
       </p>
     </React.Fragment>
   );
