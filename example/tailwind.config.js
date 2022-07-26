1ee79d8ea08790aa55bf349f5e1b78be9963e118
Dominik Ferber <dominik.ferber@gmail.com>
2022-07-26 08:20:49 +0300

    upgrade middleware example

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
