cd890a3744797a2e7ce7733612d91b4de5e048ad
Dominik Ferber <dominik.ferber@gmail.com>
2022-09-11 07:01:13 +0300

    mark api-routes and evaluate as unstable

--- example/pages/api/flags/index.ts
+++ example/pages/api/flags/index.ts
@@ -1,8 +1,8 @@
-import { createWriteHandler } from "@happykit/flags/api-route";
+import { unstable_createWriteHandler } from "@happykit/flags/api-route";
 
 export const config = { runtime: "experimental-edge" };
 
-export default createWriteHandler({
+export default unstable_createWriteHandler({
   apiKey: "",
   async setDefinitions(definitions) {
     return true;
