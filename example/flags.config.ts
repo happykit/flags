cff3a9d6adedd9d3a85265295541eb8fd91a442a
Dominik Ferber <dominik.ferber@gmail.com>
2022-09-12 08:24:51 +0300

    remove flags.config.ts from example

--- example/flags.config.ts
+++
@@ -1,9 +0,0 @@
-import { configure } from "@happykit/flags/config";
-
-configure({
-  envKey: process.env.NEXT_PUBLIC_FLAGS_ENV_KEY!,
-
-  // You can just delete this line in your own application.
-  // It's only here because we use it while working on @happykit/flags itself.
-  endpoint: process.env.NEXT_PUBLIC_FLAGS_ENDPOINT,
-});
