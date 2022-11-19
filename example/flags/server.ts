00dc73c96e86821169bf3e92dfba9e12b7e8cfd2
Dominik Ferber <dominik.ferber@gmail.com>
2022-11-19 10:07:10 +0200

    remove getDefinitions

--- example/flags/server.ts
+++ example/flags/server.ts
@@ -3,10 +3,7 @@ import {
   type GenericEvaluationResponseBody,
 } from "@happykit/flags/server";
 import { type AppFlags, config } from "./config";
-import { getDefinitions } from "./storage";
 
 export type EvaluationResponseBody = GenericEvaluationResponseBody<AppFlags>;
-export const getFlags = createGetFlags<AppFlags>(config, {
-  getDefinitions,
-  serverTiming: true,
-});
+
+export const getFlags = createGetFlags<AppFlags>(config);
