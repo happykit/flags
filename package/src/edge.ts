6f11d8174abb1187582e316587b50c874536e002
Bryce Kalow <bkalow@hashicorp.com>
2022-12-13 11:55:01 -0600

    adds a test case

--- package/src/edge.ts
+++ package/src/edge.ts
@@ -67,13 +67,13 @@ export function createGetEdgeFlags<F extends Flags>(
     let visitorKeyFromCookie;
     if (typeof options.request.cookies.get === "function") {
       const fromCookiesGet = options.request.cookies.get("hkvk");
-      
-      // In Next.js 13, the value returned from cookies.get() is an object with the type: { name: string, value: string }
+
+      // @ts-expect-error -- In Next.js 13, the value returned from cookies.get() is an object with the type: { name: string, value: string }
       visitorKeyFromCookie = typeof fromCookiesGet === 'string' ? fromCookiesGet : fromCookiesGet?.value;
     } else {
       // backwards compatible for when cookies was { [key: string]: string; }
       // in Next.js
-      visitorKeyFromCokoie = (options.request.cookies as any).hkvk || null;
+      visitorKeyFromCookie = (options.request.cookies as any).hkvk || null;
     }
 
     // When using server-side rendering and there was no visitor key cookie,
