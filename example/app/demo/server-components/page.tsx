2eb182ca7ba3762c63bd47be1b8b31a4f524c7bb
Dominik Ferber <dominik.ferber@gmail.com>
2025-12-06 22:53:46 +0200

    fix

--- example/app/demo/server-components/page.tsx
+++ example/app/demo/server-components/page.tsx
@@ -5,30 +5,31 @@ import { cookies } from "next/headers";
 
 const title = "Server Components";
 export const metadata = {
-  title: `${title} · HappyKit Flags Documentation`,
-  description: "Feature Flags for Next.js",
+	title: `${title} · HappyKit Flags Documentation`,
+	description: "Feature Flags for Next.js",
 };
 
 export default async function ServerComponentPage() {
-  const visitorKey = cookies().get("hkvk")?.value;
-  const flags = await getFlags({ visitorKey });
+	const jar = await cookies();
+	const visitorKey = jar.get("hkvk")?.value;
+	const flags = await getFlags({ visitorKey });
 
-  return (
-    <Content
-      title={title}
-      source={`https://github.com/happykit/flags/blob/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}/example/app/demo/server-components/page.tsx`}
-    >
-      <article className="py-4 prose max-w-prose">
-        <p>
-          This demo shows how to use <code>@happykit/flags</code> for server
-          components in Next.js App Router.
-        </p>
-        <p>
-          Since this page is rendered on the server only, there is no{" "}
-          <code>flagBag</code>. Instead, the values are shown directly.
-        </p>
-        <Result key="server-components" label="Flags" value={flags} />
-      </article>
-    </Content>
-  );
+	return (
+		<Content
+			title={title}
+			source={`https://github.com/happykit/flags/blob/${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}/example/app/demo/server-components/page.tsx`}
+		>
+			<article className="py-4 prose max-w-prose">
+				<p>
+					This demo shows how to use <code>@happykit/flags</code> for server
+					components in Next.js App Router.
+				</p>
+				<p>
+					Since this page is rendered on the server only, there is no{" "}
+					<code>flagBag</code>. Instead, the values are shown directly.
+				</p>
+				<Result key="server-components" label="Flags" value={flags} />
+			</article>
+		</Content>
+	);
 }
--- example/components/Nav.tsx
+++ example/components/Nav.tsx
@@ -1,90 +1,90 @@
 import * as React from "react";
 
 export function Nav({
-  NavLink,
+	NavLink,
 }: {
-  NavLink: (props: {
-    href: string;
-    children: React.ReactNode;
-    indent?: boolean | undefined;
-  }) => JSX.Element;
+	NavLink: (props: {
+		href: string;
+		children: React.ReactNode;
+		indent?: boolean | undefined;
+	}) => React.ReactElement;
 }) {
-  return (
-    <React.Fragment>
-      <div>
-        <NavLink href="/">Introduction</NavLink>
-      </div>
-      <div>
-        <NavLink href="/demo/basic-usage">Basic Usage</NavLink>
-      </div>
-      <div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
-        App Router
-      </div>
-      <div className="space-y-1 list-disc">
-        <NavLink indent href="/demo/client-components">
-          Client Components
-        </NavLink>
-        <NavLink indent href="/demo/server-components">
-          Server Components
-        </NavLink>
-      </div>
-      <div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
-        Pages Router
-      </div>
-      <div className="space-y-1 list-disc">
-        <NavLink indent href="/demo/client-side-rendering">
-          Client-Side Rendering
-        </NavLink>
-        <NavLink indent href="/demo/server-side-rendering-pure">
-          Server-Side Rendering (Pure)
-        </NavLink>
-        <NavLink indent href="/demo/server-side-rendering-hybrid">
-          Server-Side Rendering (Hybrid)
-        </NavLink>
-        <NavLink indent href="/demo/static-site-generation-pure">
-          Static Site Generation (Pure)
-        </NavLink>
-        <NavLink indent href="/demo/static-site-generation-hybrid">
-          Static Site Generation (Hybrid)
-        </NavLink>
-        <NavLink indent href="/demo/middleware">
-          Middleware
-        </NavLink>
-      </div>
-      <div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
-        Targeting and Rules
-      </div>
-      <div className="space-y-1">
-        <NavLink indent href="/demo/targeting-by-visitor-key">
-          Targeting by Visitor Key
-        </NavLink>
-        <NavLink indent href="/demo/targeting-by-user">
-          Targeting by User
-        </NavLink>
-        <NavLink indent href="/demo/targeting-by-traits">
-          Targeting by Traits
-        </NavLink>
-      </div>
-      <div>
-        <NavLink href="/demo/rollouts">Rollouts</NavLink>
-      </div>
-      <div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
-        Options and Patterns
-      </div>
-      <div className="space-y-1">
-        <NavLink indent href="/demo/disabled-revalidation">
-          Disabled Revalidation
-        </NavLink>
-        <NavLink indent href="/demo/context">
-          Context
-        </NavLink>
-      </div>
-      <div>
-        <NavLink href="/demo/dynamics">Dynamics</NavLink>
-      </div>
-      <div>
-        <NavLink href="/docs/public-api">Public API</NavLink>
-      </div>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<div>
+				<NavLink href="/">Introduction</NavLink>
+			</div>
+			<div>
+				<NavLink href="/demo/basic-usage">Basic Usage</NavLink>
+			</div>
+			<div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
+				App Router
+			</div>
+			<div className="space-y-1 list-disc">
+				<NavLink indent href="/demo/client-components">
+					Client Components
+				</NavLink>
+				<NavLink indent href="/demo/server-components">
+					Server Components
+				</NavLink>
+			</div>
+			<div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
+				Pages Router
+			</div>
+			<div className="space-y-1 list-disc">
+				<NavLink indent href="/demo/client-side-rendering">
+					Client-Side Rendering
+				</NavLink>
+				<NavLink indent href="/demo/server-side-rendering-pure">
+					Server-Side Rendering (Pure)
+				</NavLink>
+				<NavLink indent href="/demo/server-side-rendering-hybrid">
+					Server-Side Rendering (Hybrid)
+				</NavLink>
+				<NavLink indent href="/demo/static-site-generation-pure">
+					Static Site Generation (Pure)
+				</NavLink>
+				<NavLink indent href="/demo/static-site-generation-hybrid">
+					Static Site Generation (Hybrid)
+				</NavLink>
+				<NavLink indent href="/demo/middleware">
+					Middleware
+				</NavLink>
+			</div>
+			<div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
+				Targeting and Rules
+			</div>
+			<div className="space-y-1">
+				<NavLink indent href="/demo/targeting-by-visitor-key">
+					Targeting by Visitor Key
+				</NavLink>
+				<NavLink indent href="/demo/targeting-by-user">
+					Targeting by User
+				</NavLink>
+				<NavLink indent href="/demo/targeting-by-traits">
+					Targeting by Traits
+				</NavLink>
+			</div>
+			<div>
+				<NavLink href="/demo/rollouts">Rollouts</NavLink>
+			</div>
+			<div className="bg-white text-gray-600 group w-full flex items-center pl-7 pr-2 py-2 text-sm font-medium rounded-md">
+				Options and Patterns
+			</div>
+			<div className="space-y-1">
+				<NavLink indent href="/demo/disabled-revalidation">
+					Disabled Revalidation
+				</NavLink>
+				<NavLink indent href="/demo/context">
+					Context
+				</NavLink>
+			</div>
+			<div>
+				<NavLink href="/demo/dynamics">Dynamics</NavLink>
+			</div>
+			<div>
+				<NavLink href="/docs/public-api">Public API</NavLink>
+			</div>
+		</React.Fragment>
+	);
 }
